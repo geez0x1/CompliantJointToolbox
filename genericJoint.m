66486cb03cbc81065db553919bc4b6e5908f7705
Jorn Malzahn <jorn.malzahn@iit.it>
2016-06-10 14:02:07 +0200

    updated header

--- genericJoint.m
+++ genericJoint.m
@@ -10,27 +10,30 @@
 %
 %
 % Author::
-%  Joern Malzahn, jorn.malzahn@iit.it
-%  Wesley Roozing, wesley.roozing@iit.it
+%  Joern Malzahn
+%  Wesley Roozing
 %
-% See also jointBuilder, WMBigJoint.
+% See also jointBuilder.
 
 % Copyright (C) 2016, by Joern Malzahn, Wesley Roozing
 %
 % This file is part of the Compliant Joint Toolbox (CJT).
 %
 % CJT is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Lesser General Public License as published by
+% it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 %
 % CJT is distributed in the hope that it will be useful, but WITHOUT ANY
 % WARRANTY; without even the implied warranty of MERCHANTABILITY or
-% FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
 % License for more details.
 %
-% You should have received a copy of the GNU Lesser General Public License
+% You should have received a copy of the GNU General Public License
 % along with CJT. If not, see <http://www.gnu.org/licenses/>.
+%
+% For more information on the toolbox and contact to the authors visit
+% <https://github.com/geez0x1/CompliantJointToolbox>
 
 classdef genericJoint < handle
     
--- jointBuilder.m
+++ jointBuilder.m
@@ -1,3 +1,47 @@
+% JOINTBUILDER Class use to create joint models.
+%
+% Joint models consist of linear and nonlinear dynamics equations along
+% with a parameter set. The number and complexity of the nonlinear effects 
+% to be considered vary for a particular joint, depending on the
+% respecrtive objective. The same principal dynamics lead to diverse
+% results when parameterized differently. 
+%
+% This class allows to create dynamics models with arbitrary combinations
+% of the provided dynamics templates and parameter sets. It automatically
+% assembles those in a class file implementic a genericJoint.
+%
+% Notes::
+%
+%
+% Examples::
+%
+%
+% Author::
+%  Joern Malzahn
+%  Wesley Roozing
+%
+% See also genericJoint.
+
+% Copyright (C) 2016, by Joern Malzahn, Wesley Roozing
+%
+% This file is part of the Compliant Joint Toolbox (CJT).
+%
+% CJT is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% CJT is distributed in the hope that it will be useful, but WITHOUT ANY
+% WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+% License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with CJT. If not, see <http://www.gnu.org/licenses/>.
+%
+% For more information on the toolbox and contact to the authors visit
+% <https://github.com/geez0x1/CompliantJointToolbox>
+
 classdef jointBuilder
     %JOINTBUILDER Builds joint model files
     
