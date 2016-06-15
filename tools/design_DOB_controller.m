336a6c4034b4d378eaa10a960b0162c0ab9c0ade
Jorn Malzahn <jorn.malzahn@iit.it>
2016-06-15 17:37:24 +0200

    Renaming and header

--- tools/design_DOB_controller.m
+++ tools/design_DOB_controller.m
@@ -1,16 +1,73 @@
-%% [Pc, Q_td, Q_ff, PQ_td, PQ_ff] = Paine_analytical(jointName, Kp, Ki, Kd, N [, pid_form, ff_comp_switch, f_c_FF, f_c_DOB])
+% DESIGN_DOB_CONTROLLER Designs all parameters of a DOB based torque control
+% scheme.
+%
+% [Pc, Q_td, Q_ff, PQ_td, PQ_ff] = design_DOB_controller(jointName, Kp, Ki, Kd, N [, pid_form, ff_comp_switch, f_c_FF, f_c_DOB])
+%
 % This function calculates the approximated closed-loop transfer function
 % Pc, low-pass Q-filters, and the inverted models for a DOB with premulti-
 % plication control scheme. This is the standalone code, that does not
-% require measurement data from a simulink model to design the model.
+% require measurement data from a Simulink model to design the model.
 % Instead, it uses a linear state-space model to estimate the transfer
 % function of the plant, thus the results are fundamentally different from
 % the experimental version.
 % This function returns the approximated closed-loop transfer function
 % Pc, DOB filter Q_td, feed-forward filter Q_ff, DOB plant inversion +
 % filter PQ_td, and feed-forward plant in version + filter PQ_ff.
+%
+% Inputs::
+%   jointName: Joint class name
+%   Kp: Inner control loop proportional gain
+%   Ki: Inner control loop integral gain
+%   Kd: Inner control loop derivative gain
+%   N: DOB order
+%   pid_form: Flag that determins if PID controller is given in product or
+%             summation form
+%   ff_comp_switch: Flag that determins if torque feedforward is active or
+%                   not (default: true)
+%   f_c_FF:  Feed-forward cutoff frequency [Hz] (default: 40)
+%   f_c_DOB:  DOB cutoff frequency [Hz] (default: 60)
+%
+% Outputs::
+%   Pc: Estimated closed-loop transfer function
+%   Q_td: DOB Q-filter
+%   Q_ff: Feed-forward Q-filter
+%   PQ_td: Inverted plant + DOB Q-filter
+%   PQ_ff: Inverted plant + Feed-forward Q-filter
+%
+% Notes::
+%
+%
+% Examples::
+%
+%
+% Author::
+%  Joern Malzahn
+%  Wesley Roozing
+%
+% See also getLinearDOB, getKalman.
 
-function [Pc, Q_td, Q_ff, PQ_td, PQ_ff] = Paine_analytical(jointName, Kp, Ki, Kd, N, pid_form, ff_comp_switch, f_c_FF, f_c_DOB)
+% Copyright (C) 2016, by Joern Malzahn, Wesley Roozing
+%
+% This file is part of the Compliant Joint Toolbox (CJT).
+%
+% CJT is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% CJT is distributed in the hope that it will be useful, but WITHOUT ANY
+% WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+% License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with CJT. If not, see <http://www.gnu.org/licenses/>.
+%
+% For more information on the toolbox and contact to the authors visit
+% <https://github.com/geez0x1/CompliantJointToolbox>
+
+
+function [Pc, Q_td, Q_ff, PQ_td, PQ_ff] = design_DOB_controller(jointName, Kp, Ki, Kd, N, pid_form, ff_comp_switch, f_c_FF, f_c_DOB)
 
     %% Default parameters
     if (~exist('pid_form', 'var'))
@@ -138,7 +195,8 @@ function [Pc, Q_td, Q_ff, PQ_td, PQ_ff] = Paine_analytical(jointName, Kp, Ki, Kd
 
 
     %% Save results so we don't have to recalculate them all the time
-    save('Paine_analytical_results.mat', 'Pc', 'Q_td', 'Q_ff', 'PQ_td', 'PQ_ff');
-    disp('Data saved to Paine_analytical_results.mat');
+    fName = 'design_results.mat';
+    save(fName, 'Pc', 'Q_td', 'Q_ff', 'PQ_td', 'PQ_ff');
+    disp(['Data saved to ' fName]);
     
 end
