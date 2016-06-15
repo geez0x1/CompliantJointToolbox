ce2056a7b37d36c1c416597ab5b667087977d23a
Jorn Malzahn <jorn.malzahn@iit.it>
2016-06-15 16:59:17 +0200

    headers

--- templates/getDynamicsMatrices.m
+++ templates/getDynamicsMatrices.m
@@ -1,3 +1,51 @@
+%GETDYNAMICSMATRICES Template for obtaining state space matrices for the linear dynamics.
+%
+% [A, B, C, I, D, K] = jointObj.getDynamicsMatrices
+%
+% jointObj is the instance of the joint class object for which this
+% function has been called.
+%
+% Outputs::
+%
+%
+%   A: System matrix
+%   B: Input matrix
+%   C: Output matrix
+%   I: Inertia matrix
+%   D: Damping matrix
+%   K: Stiffness matrix
+%
+% Notes::
+%
+%
+% Examples::
+%
+% Author::
+%  Joern Malzahn
+%  Wesley Roozing
+%
+% See also genericJoint, full_dyn, getNonlinearDynamics.
+
+% Copyright (C) 2016, by Joern Malzahn, Wesley Roozing
+%
+% This file is part of the Compliant Joint Toolbox (CJT).
+%
+% CJT is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% CJT is distributed in the hope that it will be useful, but WITHOUT ANY
+% WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+% License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with CJT. If not, see <http://www.gnu.org/licenses/>.
+%
+% For more information on the toolbox and contact to the authors visit
+% <https://github.com/geez0x1/CompliantJointToolbox>
+
 %%__________________________________________________________________
 %% Get dynamics matrices
 function [A, B, C, I, D, K] = getDynamicsMatrices(obj)
