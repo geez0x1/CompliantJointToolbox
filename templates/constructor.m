ddc185b36513d19a2adc95432fd3475f5759b6af
Wesley Roozing <wesley.roozing@iit.it>
2016-06-16 10:59:47 +0200

    Fixed new function headers in templates/ breaking the jointBuilder (requiring double % for sprintf)

--- templates/constructor.m
+++ templates/constructor.m
@@ -1,33 +1,30 @@
-%CONSTRUCTOR Template for joint class constructor
-%
-% Author::
-%  Joern Malzahn
-%  Wesley Roozing
-%
-% See also genericJoint, jointBuilder, getNonlinearDynamics.
+%%CONSTRUCTOR Template for joint class constructor
+%%
+%% Author::
+%%  Joern Malzahn
+%%  Wesley Roozing
+%%
+%% See also genericJoint, jointBuilder, getNonlinearDynamics.
 
-% Copyright (C) 2016, by Joern Malzahn, Wesley Roozing
-%
-% This file is part of the Compliant Joint Toolbox (CJT).
-%
-% CJT is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-%
-% CJT is distributed in the hope that it will be useful, but WITHOUT ANY
-% WARRANTY; without even the implied warranty of MERCHANTABILITY or
-% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
-% License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with CJT. If not, see <http://www.gnu.org/licenses/>.
-%
-% For more information on the toolbox and contact to the authors visit
-% <https://github.com/geez0x1/CompliantJointToolbox>
-
-%%__________________________________________________________________
-%% Constructor
+%% Copyright (C) 2016, by Joern Malzahn, Wesley Roozing
+%%
+%% This file is part of the Compliant Joint Toolbox (CJT).
+%%
+%% CJT is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% CJT is distributed in the hope that it will be useful, but WITHOUT ANY
+%% WARRANTY; without even the implied warranty of MERCHANTABILITY or
+%% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+%% License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with CJT. If not, see <http://www.gnu.org/licenses/>.
+%%
+%% For more information on the toolbox and contact to the authors visit
+%% <https://github.com/geez0x1/CompliantJointToolbox>
 function this = %s(verbose, debug)
     %% Verbosity and debugging
     params.verbose		= 0;
--- templates/getNonlinearDynamics_none.m
+++ templates/getNonlinearDynamics_none.m
@@ -1,52 +1,48 @@
-%GETNONLINEARDYNAMICS Template for a dummy implementation for nonlinear
-%dynamics expressions.
-%
-% tau = jointObj.getNonlinearDynamics(x)
-%
-% jointObj is the instance of the joint class object for which this
-% function has been called.
-%
-% Inputs:: 
-%   x: state vector
-%
-% Outputs::
-%   tau: Generalized force vector
-%
-% Notes::
-%   The function created from this template just outputs a zero vector with 
-%   dimensions equal to the state vector.
-%
-% Examples::
-%
-% Author::
-%  Joern Malzahn
-%  Wesley Roozing
-%
-% See also genericJoint, full_dyn, getNonlinearDynamics.
+%%GETNONLINEARDYNAMICS Template for a dummy implementation for nonlinear
+%%dynamics expressions.
+%%
+%% tau = jointObj.getNonlinearDynamics(x)
+%%
+%% jointObj is the instance of the joint class object for which this
+%% function has been called.
+%%
+%% Inputs:: 
+%%   x: state vector
+%%
+%% Outputs::
+%%   tau: Generalized force vector
+%%
+%% Notes::
+%%   The function created from this template just outputs a zero vector with 
+%%   dimensions equal to the state vector.
+%%
+%% Examples::
+%%
+%% Author::
+%%  Joern Malzahn
+%%  Wesley Roozing
+%%
+%% See also genericJoint, full_dyn, getNonlinearDynamics.
 
-% Copyright (C) 2016, by Joern Malzahn, Wesley Roozing
-%
-% This file is part of the Compliant Joint Toolbox (CJT).
-%
-% CJT is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-%
-% CJT is distributed in the hope that it will be useful, but WITHOUT ANY
-% WARRANTY; without even the implied warranty of MERCHANTABILITY or
-% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
-% License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with CJT. If not, see <http://www.gnu.org/licenses/>.
-%
-% For more information on the toolbox and contact to the authors visit
-% <https://github.com/geez0x1/CompliantJointToolbox>
-
-
-%%__________________________________________________________________
-%% Get nonlinear dynamics
+%% Copyright (C) 2016, by Joern Malzahn, Wesley Roozing
+%%
+%% This file is part of the Compliant Joint Toolbox (CJT).
+%%
+%% CJT is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% CJT is distributed in the hope that it will be useful, but WITHOUT ANY
+%% WARRANTY; without even the implied warranty of MERCHANTABILITY or
+%% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+%% License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with CJT. If not, see <http://www.gnu.org/licenses/>.
+%%
+%% For more information on the toolbox and contact to the authors visit
+%% <https://github.com/geez0x1/CompliantJointToolbox>
 function tau = getNonlinearDynamics(obj, x)
     tau = zeros(size(x)); %% No nonlinear dynamics!
 end
