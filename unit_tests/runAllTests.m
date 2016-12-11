2e48c56b6574acf98181c3c4f848880f6d5a47c1
Jorn Malzahn <jorn.malzahn@iit.it>
2016-12-11 22:10:36 +0100

    script to run all unit tests

---
+++ unit_tests/runAllTests.m
@@ -0,0 +1,3 @@
+results_JointBuilder       = runtests('jointBuilderTest');
+results_GenericJoint       = runtests('genericJointTest');
+results_DataSheetGenerator = runtests('dataSheetGeneratorTest');
