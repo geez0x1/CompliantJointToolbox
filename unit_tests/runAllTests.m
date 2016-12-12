ec22f6b01f11ce1a3e45890f2e33980d7ef125ff
Jorn Malzahn <jorn.malzahn@iit.it>
2016-12-12 15:25:51 +0100

    Display test results after running runAllTests.m

--- unit_tests/runAllTests.m
+++ unit_tests/runAllTests.m
@@ -1,3 +1,12 @@
+%% Run Tests
 results_JointBuilder       = runtests('jointBuilderTest');
 results_GenericJoint       = runtests('genericJointTest');
 results_DataSheetGenerator = runtests('dataSheetGeneratorTest');
+
+
+%% Display Results
+clc;
+
+results_JointBuilder
+results_GenericJoint
+results_DataSheetGenerator
