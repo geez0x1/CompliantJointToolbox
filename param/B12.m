44e062e3ce7ffac0f42c5289a2ae465567e03be7
Jorn Malzahn <jorn.malzahn@iit.it>
2017-03-07 11:06:14 +0100

    Updated parameters for B11 and B12, rearranged friction and added thermal parameters

--- param/B12.m
+++ param/B12.m
@@ -1,4 +1,4 @@
-%WMBIG10K Parameter script for the WALK-MAN Big Motor B12 with 1623 Nm/rad torsion bar
+%B12 Parameter script for the WALK-MAN Big Motor B12 with 2700 Nm/rad torsion bar
 %
 % Notes::
 %  All inertiae/damping is reflected to link side using n^2
@@ -40,30 +40,26 @@ params.('k_g')      = 30e3;                      %% Gearbox stiffness [Nm/rad]
 params.('k_b')      = 2700;                      %% Torsion bar stiffness [Nm/rad]
 % Linear viscous friction
 params.('d_m')      = 0;                         %% Motor Damping [Nms/rad]
-params.('d_g')      = 2.5154;                    %% Gearbox damping [Nms/rad]
+params.('d_g')      = 12.1997;                    %% Gearbox damping [Nms/rad]
 params.('d_l')      = 0;                         %% Torsion bar damping [Nms/rad]
 % Asymmetric viscous friction
 params.('d_m_n')    = 0;                         %% Motor Damping - negative direction [Nms/rad]
-params.('d_g_n')    = 2.6940;                    %% Gearbox Damping - negative direction [Nms/rad]
+params.('d_g_n')    = 12.0650;                    %% Gearbox Damping - negative direction [Nms/rad]
 params.('d_l_n')    = 0;                         %% Torsion bar damping - negative direction [Nms/rad]
 % Linear internal viscous friction
 params.('d_mg')     = 252.5627;                  %% Gearbox internal damping [Nms/rad] (not identified)
-params.('d_gl')     = 10.3300;                   %% Torsion bar internal damping [Nms/rad] (not identified)
+params.('d_gl')     = 0;                         %% Torsion bar internal damping [Nms/rad] (not identified)
 % Coulomb friction
 params.('d_cm')     = 0;                         %% Motor Coulomb damping [Nm]
-params.('d_cg')     = 0.8163;                    %% Gearbox Coulomb damping [Nm]
+params.('d_cg')     = 3.3897;                    %% Gearbox Coulomb damping [Nm]
 params.('d_cl')     = 0;                         %% Torsion bar Coulomb damping [Nm]
 % Asymmetric Coulomb friction
 params.('d_cm_n')   = 0;                         %% Motor Coulomb damping - negative direction [Nm]
-params.('d_cg_n')   = 0.8892;                    %% Gearbox Coulomb damping - negative direction [Nm]
+params.('d_cg_n')   = 3.5562;                    %% Gearbox Coulomb damping - negative direction [Nm]
 params.('d_cl_n')   = 0;                         %% Torsion bar Coulomb damping - negative direction [Nm]
 % Stiction
 params.('d_s')      = 1.8;                       %% Break away torque [Nm]
 params.('v_s')      = 0.01;                      %% Stribeck velocity range [rad/s]
-% Cogging
-params.('cog_a1')   = 15e-3 * params.('n');      %% Cosine amplitude [Nm]
-params.('cog_a2')   = 0 * params.('n');          %% Sine amplitude [Nm]
-params.('cog_f')    = 13 * params.('n');         %% Spatial frequency [periods/revolution]
 % Misc
 params.('k_t')      = 0.0445;                    %% Torque constant [Nm/A]
 params.('r')        = 0.0885;                    %% Armature resistance [Ohm]
@@ -72,4 +68,10 @@ params.('Ts')       = 5e-4;                      %% Sampling time [s]
 % Operating/max conditions
 params.('v_0')      = 24;                        %% Operating voltage [V]
 params.('i_p')      = 80;                        %% Peak current [A]
-params.('dq_p')     = 5.86;                      %% Max. peak speed (output) [rad/s]
+params.('dq_p')     = 10.66;                      %% Max. peak speed (output) [rad/s]
+% Thermal parameters
+params.('r_th1')    = 0.29;                      %% Thermal Resistance Windings to Housing [K/W]
+params.('r_th2')    = 0.73;                      %% Thermal Resistance Housing to Air [K/W]             
+params.('T_thw')    = 0.56;                      %% Thermal Time Constant of the Windings [s]           
+params.('T_thm')    = 14456;                     %% Thermal Time Constant of the Motor [s]             
+params.('Tmp_WMax') = 155;                       %% Maximum Armature Temperature [°C]
