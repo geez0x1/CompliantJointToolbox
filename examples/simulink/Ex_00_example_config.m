594aaa78b2d86fbc4d81ee0c972305b2435d55b4
Jörn Malzahn <10830038+joernmalzahn@users.noreply.github.com>
2018-03-09 12:57:43 +0100

    added missing example config for the simulink controller example

---
+++ examples/simulink/Ex_00_example_config.m
@@ -0,0 +1,19 @@
+%% Configure Joint Model
+jb = jointBuilder;
+jb.overwrite = 1;
+
+jb.buildJoint('cjt_Orange_80_6000',...
+       'output_fixed','coulomb_asym',...
+       [],'example_joint_locked');
+
+addpath(jb.buildDir);
+
+jObj = example_joint_locked;
+
+%% Configure Chirp Source
+f_min = 0.01; % Initial frequency in Hz
+f_max = 100;  % Final frequency in Hz
+t_max = f_max; % Simulation stop time in s
+chrip_amp = 5; % A
+
+
