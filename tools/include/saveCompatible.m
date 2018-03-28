0021038d4c4631293e7b889afaa93019428c7bdf
Wesley Roozing <wesley.roozing@iit.it>
2018-03-28 08:43:11 +0200

    saveCompatible.m: Added more files, more info

--- tools/include/saveCompatible.m
+++ tools/include/saveCompatible.m
@@ -8,8 +8,15 @@ files = { ...
     'lib/cjt_library_models.mdl', ...
     'lib/cjt_library_observers.mdl', ...
     'lib/cjt_library.mdl', ...
-    'controlled.mdl', ...
+    'examples/simulink/controlBlockDevelopment_generic.mdl', ...
+    'examples/simulink/Ex_02_Current_Control.mdl', ...
+    'examples/simulink/Ex_03_I_Control_Plus_Feedforward.mdl', ...
+    'examples/simulink/openLoop.mdl', ...
+    'examples/simulink/openLoop_inputFromFile.mdl', ...
 };
+
+% Counter for number of converted files
+j = 0;
 
 % Go through each file
 for i=1:length(files)
@@ -31,9 +38,9 @@ for i=1:length(files)
         % Get new filename/path
         [pathstr,name,ext] = fileparts(file);
         if (strcmp(pathstr, ''))
-            newFile = [name '_2' ext];
+            newFile = [name '_compat' ext];
         else
-            newFile = [pathstr filesep name '_2' ext];
+            newFile = [pathstr filesep name '_compat' ext];
         end
         
         % Load old file and save standardised version to new filename
@@ -47,8 +54,14 @@ for i=1:length(files)
         if (status ~= 1)
             error(['ERR: Could not move file ''' newFile ''' to ''' file '''']);
         end
+        
+        % Increment counter
+        j = j + 1;
+        
+    else
+        disp(['Library file ''' file ''' is OK.']);
     end
     
 end
 
-disp('Finished.');
+disp(['Finished. Converted ' num2str(j) ' files.']);
