ddff9261ccc887232b7080513ef30011a17a82ec
Wesley Roozing <wesley.roozing@iit.it>
2018-03-28 10:08:13 +0200

    - Fixed simulink ex2 not using the library current controller - Fixed simulink examples not using standard 'jointObj' variable name - Fixed current controller variable names

--- examples/simulink/Ex_00_example_config.m
+++ examples/simulink/Ex_00_example_config.m
@@ -2,17 +2,19 @@
 jb = jointBuilder;
 jb.overwrite = 1;
 
-jb.buildJoint('cjt_Orange_80_6000',...
-       'output_fixed','coulomb_asym',...
-       [],'example_joint_locked');
+jb.buildJoint(  'cjt_Orange_80_6000', ...
+                'output_fixed', ...
+                'coulomb_asym', ...
+                [], ...
+                'example_joint_locked'      );
 
 addpath(jb.buildDir);
 
-jObj = example_joint_locked;
+jointObj = example_joint_locked;
 
 %% Configure Chirp Source
-f_min = 0.01; % Initial frequency in Hz
-f_max = 100;  % Final frequency in Hz
-t_max = f_max; % Simulation stop time in s
+f_min = 0.01;   % Initial frequency in Hz
+f_max = 100;    % Final frequency in Hz
+t_max = f_max;  % Simulation stop time in s
 
 
