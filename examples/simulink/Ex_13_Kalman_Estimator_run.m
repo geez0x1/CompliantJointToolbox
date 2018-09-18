b387924a875ebe114cf1f2d81162ecf8ae004b2b
Jörn Malzahn <10830038+joernmalzahn@users.noreply.github.com>
2018-09-18 11:31:32 +0200

    resolved #62

--- examples/simulink/Ex_13_Kalman_Estimator_run.m
+++ examples/simulink/Ex_13_Kalman_Estimator_run.m
@@ -27,7 +27,7 @@
 % <https://github.com/geez0x1/CompliantJointToolbox>
 
 % The text in the following line defines the display name for this Example
-% #! 12: Passivity based control with friction compensation
+% #! 13: Kalman Estimator
 
 dispText = ...
 {'-----'
